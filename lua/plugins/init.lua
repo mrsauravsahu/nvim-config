cef6779eed58c960d10479e68c97c2f9cad53ead
mrsauravsahu <mrsauravsahu@outlook.com>
2025-11-14 18:58:56 +0530

    chore: install gp

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -31,7 +31,17 @@ return {
       })
     end,
   },
+  {
+      "robitx/gp.nvim",
+      config = function()
+          local conf = {
+              -- For customization, refer to Install > Configuration in the Documentation/Readme
+          }
+          require("gp").setup(conf)
 
+          -- Setup shortcuts here (see Usage > Shortcuts in the Documentation/Readme)
+      end,
+  }
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
