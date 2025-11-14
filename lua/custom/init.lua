a4584b67942a68f4437627e2a282d7c7fc3bc221
mrsauravsahu <mrsauravsahu@outlook.com>
2025-11-14 18:49:23 +0530

    chore: update keymappings and turn off startup script

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,12 +1,11 @@
 -- Auto open NvimTree when starting Neovim
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    if vim.fn.argc() == 0 then
-      require("nvim-tree.api").tree.open()
-
-      -- Move focus back to previous buffer (or empty buffer)
-      vim.cmd("wincmd p")
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd("VimEnter", {
+--   callback = function()
+--     if vim.fn.argc() == 0 then
+--       require("nvim-tree.api").tree.open()
+--       Move focus back to previous buffer (or empty buffer)
+--       vim.cmd("wincmd p")
+--     end
+--   end,
+-- })
 
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,9 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({"n", "i", "t"}, "<C-\\>", function()
+  vim.cmd("NvimTreeToggle")
+end, { noremap = true, silent = true })
+
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
