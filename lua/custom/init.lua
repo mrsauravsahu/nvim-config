220699eef64c621726015eee369e535afa82b9f7
mrsauravsahu <mrsauravsahu@outlook.com>
2025-11-18 10:37:47 +0530

    chore: allow folding with za

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,11 +1,17 @@
 -- Auto open NvimTree when starting Neovim
--- vim.api.nvim_create_autocmd("VimEnter", {
---   callback = function()
---     if vim.fn.argc() == 0 then
---       require("nvim-tree.api").tree.open()
---       Move focus back to previous buffer (or empty buffer)
---       vim.cmd("wincmd p")
---     end
---   end,
--- })
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 0 then
+      -- require("nvim-tree.api").tree.open()
+      -- Move focus back to previous buffer (or empty buffer)
+      -- vim.cmd("wincmd p")
+      vim.cmd("set foldlevelstart=99")
+      vim.cmd("set foldmethod=indent")
+
+    end
+  end,
+})
+
+-- require "lsp.init"
 
