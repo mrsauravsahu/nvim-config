94ef9c3e5d389870d1d74a174f15aef3703a1f25
mrsauravsahu <mrsauravsahu@outlook.com>
2025-11-18 10:55:42 +0530

    chore: install autosession

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -80,6 +80,18 @@ return {
       -- Setup shortcuts here (see Usage > Shortcuts in the Documentation/Readme)
     end,
   },
+  {
+    "rmagatti/auto-session",
+    lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      -- log_level = 'debug',
+    },
+  }
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
