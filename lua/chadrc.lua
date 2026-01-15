651b58512530310229ecf60e0f3714758871cf6e
mrsauravsahu <mrsauravsahu@outlook.com>
2026-01-15 06:12:04 +0530

    chore: render characters like trailing spaces new lines etc

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,7 +6,7 @@
 local M = {}
 
 M.base46 = {
-	theme = "aylin",
+	theme = "aquarium",
 
 	hl_override = {
     Comment = { italic = true },
--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -15,3 +15,20 @@ vim.api.nvim_create_autocmd("VimEnter", {
 
 -- require "lsp.init"
 
+-- Enable list mode to show non-printable characters
+vim.opt.list = true
+
+-- Define how different whitespace characters are rendered
+-- space:· displays regular spaces as middle dots
+-- tab:»- displays tabs as » followed by hyphens to fill the space
+-- trail:· displays trailing spaces as middle dots
+-- nbsp:+ displays non-breaking spaces as plus signs
+vim.opt.listchars = {
+  space = '·',
+  tab = '» ',
+  trail = '·',
+  nbsp = '+',
+  extends = '⟩',
+  precedes = '⟨'
+}
+
