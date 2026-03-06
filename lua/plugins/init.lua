afe836567600ff973ba69670885d2ca15de43c54
mrsauravsahu <mrsauravsahu@outlook.com>
2026-03-06 17:27:40 +0530

    chore: update config for GP.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -55,31 +55,31 @@ return {
     "NvChad/nvterm",
     config = function()
       require("nvterm").setup({
-      terminals = {
-        shell = vim.o.shell,
-        list = {},
-        type_opts = {
-          float = {
-            relative = 'editor',
-            row = 0.3,
-            col = 0.25,
-            width = 0.5,
-            height = 0.4,
-            border = "single",
-          },
-          horizontal = { location = "rightbelow", split_ratio = 1 },
-          vertical = { location = "rightbelow", split_ratio = .5 },
-        }
-      },
-      behavior = {
-        autoclose_on_quit = {
-          enabled = false,
-          confirm = true,
+        terminals = {
+          shell = vim.o.shell,
+          list = {},
+          type_opts = {
+            float = {
+              relative = 'editor',
+              row = 0.3,
+              col = 0.25,
+              width = 0.5,
+              height = 0.4,
+              border = "single",
+            },
+            horizontal = { location = "rightbelow", split_ratio = 1 },
+            vertical = { location = "rightbelow", split_ratio = .5 },
+          }
         },
-        close_on_exit = true,
-        auto_insert = true,
-      },
-    })
+        behavior = {
+          autoclose_on_quit = {
+            enabled = false,
+            confirm = true,
+          },
+          close_on_exit = true,
+          auto_insert = true,
+        },
+      })
     end,
   },
   {
@@ -112,50 +112,83 @@ return {
     lazy = false,
     config = function()
       local conf = {
-      agents = {
-        {
-          name = "ChatGPT-o3-mini",
-          disable = true
-        },
-        {
-          name = "gpt5",
-          model = "gpt-5-nano-2025-08-07",
-          provider = "openai",
-          system_prompt = "You are a Senior Software Engineer.",
-          chat = true,
-          command = true,
-        },
-        {
-          provider = "ollama",
-          name = "ollama-qwen",
-          chat = true,
-          -- string with model name or table with model name and parameters
-          model = {
-            model = "qwen2.5-coder:latest",
-            temperature = 0,
-            top_p = 1,
-            min_p = 0.05,
+        agents = {
+          { disable = true, name = "ChatGPT4o" },
+          { disable = true, name = "ChatGPT4o-mini" },
+          { disable = true, name = "ChatGPT-o3-mini" },
+          { disable = true, name = "ChatCopilot" },
+          { disable = true, name = "ChatGemini" },
+          { disable = true, name = "ChatPerplexityLlama3.1-8B" },
+          { disable = true, name = "ChatClaude-3-7-Sonnet" },
+          { disable = true, name = "ChatClaude-Sonnet-4-Thinking" },
+          { disable = true, name = "ChatClaude-3-5-Haiku" },
+          { disable = true, name = "ChatOllamaLlama3.1-8B" },
+          { disable = true, name = "ChatQwen3-8B" },
+          { disable = true, name = "ChatLMStudio" },
+          { disable = true, name = "CodeGPT4o" },
+          { disable = true, name = "CodeGPT-o3-mini" },
+          { disable = true, name = "CodeGPT4o-mini" },
+          { disable = true, name = "CodeCopilot" },
+          { disable = true, name = "CodeGemini" },
+          { disable = true, name = "CodePerplexityLlama3.1-8B" },
+          { disable = true, name = "CodeClaude-3-7-Sonnet" },
+          { disable = true, name = "CodeClaude-3-5-Haiku" },
+          { disable = true, name = "CodeOllamaLlama3.1-8B" },
+          {
+            name = "gpt5nano",
+            model = "gpt-5-nano-2025-08-07",
+            provider = "openai",
+            system_prompt = "You are a Senior Software Engineer.",
+            chat = true,
+            command = true,
           },
-          -- system prompt (use this to specify the persona/role of the AI)
-          system_prompt = "You are a Senior Software Engineer.",
-          },
+          -- {
+          --   provider = "ollama",
+          --   name = "ollama-qwen",
+          --   chat = true,
+          --   -- string with model name or table with model name and parameters
+          --   model = {
+          --     model = "qwen2.5-coder:latest",
+          --     temperature = 0,
+          --     top_p = 1,
+          --     min_p = 0.05,
+          --   },
+          --   -- system prompt (use this to specify the persona/role of the AI)
+          --   system_prompt = "You are a Senior Software Engineer.",
+          -- },
           {
             provider = "ollama",
-            name = "ollama-llama",
+            name = "llama",
             chat = true,
+            commmand = true,
             -- string with model name or table with model name and parameters
             model = {
-              model = "llama3.1:8b",
+              model = "llama3.2-vision:11b",
               temperature = 0,
               top_p = 1,
               min_p = 0.05,
             },
             -- system prompt (use this to specify the persona/role of the AI)
             system_prompt = "You are a Senior Software Engineer.",
-          }
-      },
-        default_command_agent = "ollama-llama",
-        default_chat_agent = "ollama-llama",
+          },
+          {
+            provider = "anthropic",
+            name = "haiku",
+            chat = true,
+            command = true,
+            -- string with model name or table with model name and parameters
+            model = {
+              model = "claude-haiku-4-5-20251001",
+              temperature = 0,
+              -- top_p = 1,
+              -- min_p = 0.05,
+            },
+            -- system prompt (use this to specify the persona/role of the AI)
+            system_prompt = "You are a Senior Software Engineer.",
+          },
+        },
+        default_command_agent = "llama",
+        default_chat_agent = "llama",
         -- },
         -- For customization, refer to Install > Configuration in the Documentation/Readme
         providers = {
@@ -167,11 +200,13 @@ return {
             endpoint = os.getenv("OPENAI_API_HOST"),
             secret = os.getenv("OPENAI_API_KEY"),
           },
+          anthropic = {
+            endpoint = os.getenv('ANTHROPIC_API_HOST'),
+            secret = os.getenv("ANTHROPIC_API_KEY"),
+          },
         }
       }
       require("gp").setup(conf)
-
-      -- Setup shortcuts here (see Usage > Shortcuts in the Documentation/Readme)
     end,
   },
   {
@@ -187,16 +222,16 @@ return {
     },
   },
   {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
         "vim",
         "lua",
         "vimdoc",
         "html",
         "css"
-  		},
-  	},
+      },
+    },
   },
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
