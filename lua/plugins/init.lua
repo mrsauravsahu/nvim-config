b69727b06676b4c1c16a38657a7e5e6a03b145b0
mrsauravsahu <mrsauravsahu@outlook.com>
2026-07-17 21:23:00 +0530

    chore: add binding for Git Fugitive

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -10,7 +10,22 @@ return {
         desc = "Launch Git fugitive",
         silent = true,
         nowait = true
-      }
+      },
+      {
+      "<leader>gg",
+      function()
+        for _, win in ipairs(vim.api.nvim_list_wins()) do
+          local buf = vim.api.nvim_win_get_buf(win)
+          if vim.bo[buf].filetype == "fugitive" then
+            vim.api.nvim_win_close(win, true)
+            return
+          end
+        end
+        vim.cmd "Git"
+      end,
+      desc = "Toggle Git fugitive",
+      silent = true,
+    },
     }
   },
   {
